690cb5b4b0f388ae38f08a74d466a91963a0bbc3
Sori <sorinapopaan@gmail.com>
2023-11-14 22:54:43 +0200

    Home Fragment Modification and Interface Implementation
    modified the fragment_home.xml to use viewModel and updated the interface

--- app/src/main/java/com/example/supernaturaltrivia/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/supernaturaltrivia/ui/home/HomeViewModel.kt
@@ -6,8 +6,24 @@ import androidx.lifecycle.ViewModel
 
 class HomeViewModel : ViewModel() {
 
+    val onEasyButtonClicked = MutableLiveData(false)
+    val onMediuButtonClicked = MutableLiveData(false)
+    val onHardButtonClicked = MutableLiveData(false)
+
     private val _text = MutableLiveData<String>().apply {
         value = "This is home Fragment"
     }
     val text: LiveData<String> = _text
+
+    fun onClickEasyButton(){
+        onEasyButtonClicked.value=true
+    }
+
+    fun onClickMediumButton(){
+        onEasyButtonClicked.value=true
+    }
+
+    fun onClickHardButton(){
+        onEasyButtonClicked.value=true
+    }
 }
